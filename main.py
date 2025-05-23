52f5ba85c5da569ff427128b56942ba6f90981d3
Maria Luísa Barreto de Campos Ribeiro <malubacari@gmail.com>
2025-05-23 11:20:21 -0300

    Finished the basics of the file organizer project

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
+from datetime import datetime
+import shutil
 
 root = os.getcwd()
 disorganized_path = f'{root}/disorganizedFiles'
+organized_path = f'{root}/organizedFiles'
 
 files = os.listdir(disorganized_path)
 
 
 for file in files:
-    while os.path.isdir(file):
-        pass
     extension = file.split('.')[1].upper()
-    print(extension)
+    path = f'{organized_path}/{extension}'
 
+    timestamp = os.path.getmtime(path)
+    mod_date = datetime.fromtimestamp(timestamp).date()
+
+    os.makedirs(path, exist_ok=True)
+
+    complete_path = f'{path}/{mod_date}'
+    os.makedirs(complete_path, exist_ok=True)
+
+    origin = f"{disorganized_path}/{file}"
+    destination = f'{complete_path}/{file}'
+
+    shutil.move(origin, destination)
